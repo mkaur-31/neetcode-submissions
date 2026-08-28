f0261c0d3a68f914cdb11353769ced8dc40ad912
Mani <m.kaur777@yahoo.com>
2026-08-28 16:10:07 -0400

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = Node()
+        
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = Node()
+            cur = cur.children[c]
+        cur.isEnd = True
+        
+
+    def search(self, word: str) -> bool:
+        def dfs(j, root):
+            cur = root
+            for i in range(j, len(word)):
+                c = word[i]
+                if c != '.':
+                    if c not in cur.children:
+                        return False
+                    cur = cur.children[c]
+                else:
+                    for child in cur.children.values():
+                        if dfs(i+1, child):
+                            return True
+                    return False
+            return cur.isEnd
+        return dfs(0,self.root)
+
+        
